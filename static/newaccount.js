73d096c2d9605d05eeb0bdf886cab726a6400bba
Alex Sirr <sirra@uw.edu>
2016-05-29 21:01:44 -0700

    fix missing .val()

--- static/newaccount.js
+++ static/newaccount.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     });
 
     function newaccount(){
-        $.post("/Qnewaccount", {fname: $("#fname"), lname: $("#lname").val(), email: $("#email").val(), phone: $("#phone").val(), password: $("#password").val()})
+        $.post("/Qnewaccount", {fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val(), phone: $("#phone").val(), password: $("#password").val()})
         .done(function(data){
           if(data.result == "failed"){
             console.log(data)

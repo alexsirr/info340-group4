15f0f11206e3cae9645ba7e9133ec6c001cb7015
alexsirr <sirra@uw.edu>
2016-05-30 11:57:30 -0700

    add more js

--- static/newaccount.js
+++ static/newaccount.js
@@ -8,8 +8,20 @@ $(document).ready(function(){
     function newaccount(){
 		var vals = {fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val(), phone: $("#phone").val(), password: $("#password").val()};
 		console.log(vals);
-        $.post("/Qnewaccount", vals).done(function(data) {
-			$("#result").append(data);
+    	for (var key in vals) {
+			if (vals[key].length == 0) {
+				$("#result").text("Please Complete All Items");
+				return;
+        	}
+      	}
+      
+      	if (!(/(\d{3}-){2}\d{4}/.test(vals["phone"]))) {
+      		$("#result").text("Phone Number Must Be in Format ###-###-#####");
+        	return;
+      	}
+      
+		$.post("/Qnewaccount", vals).done(function(data) {
+			$("#result").text(data);
 		}, "html");
     }
 })

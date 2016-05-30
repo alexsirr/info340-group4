6976ddc170fbfd4597c4dd74e86578092d391176
alexsirr <sirra@uw.edu>
2016-05-30 10:58:17 -0700

    fix up new account page

--- static/newaccount.js
+++ static/newaccount.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
-    document.getElementById("newaccount").addEventListener("click", function() {
-        console.log("blah");
-        newaccount();
-    });
+    $("#newaccount").click(function() {
+		newaccount();
+	})
 
     function newaccount(){
         $.post("/Qnewaccount", {fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val(), phone: $("#phone").val(), password: $("#password").val()})

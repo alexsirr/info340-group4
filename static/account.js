83767124021ebd79782842934690b4fef25600d4
Alex Sirr <sirra@uw.edu>
2016-05-29 23:15:48 -0700

    fix js because i am dumb

--- static/account.js
+++ static/account.js
@@ -1,7 +1,11 @@
-$(function(){
+$(document).ready(function(){
 
-    $.get("/QuserInfo", function(data){
-        $("#user-info").append(data);
-    }, "html")
+    document.getElementById("newaccount").addEventListener("click", function() {
+        console.log("blah");
+        newaccount();
+    });
 
+    function newaccount(){
+        $.post("/Qnewaccount", {fname: $("#fname").val(), lname: $("#lname").val(), email: $("#email").val(), phone: $("#phone").val(), password: $("#password").val()})
+    }
 })
